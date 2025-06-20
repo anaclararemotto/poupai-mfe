028e29412cdad036a85f4f8a4d6f0608ad8d5f92
Clara <clararemotto@gmail.com>
2025-06-20 17:31:43 -0300

    feat: create actions buttons

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeAccount } from "../home-account/home-account";
+import { Actions } from "../../../shared/actions/actions";
+import { CardInfo } from "../../../shared/card-info/card-info";
 
 @Component({
   selector: 'app-home',
-  imports: [HomeAccount],
+  imports: [HomeAccount, Actions, CardInfo],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
