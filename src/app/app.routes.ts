443d586d7b877260314f96f2a4dd4cd2b94b8c14
Clara <clararemotto@gmail.com>
2025-06-20 16:02:04 -0300

    feat: create home module and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./features/home/home-module').then((m) => m.HomeModule),
+  },
+  { path: '**', redirectTo: '/login' },
+
+];
