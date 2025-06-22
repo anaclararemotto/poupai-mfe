d3f98453f51482d7d56f782990be26fc0b90ecec
Clara <clararemotto@gmail.com>
2025-06-22 15:17:37 -0300

    feat: create transfer button

--- src/app/features/transactions/transactions/transactions.ts
+++ src/app/features/transactions/transactions/transactions.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TransactionsAccount } from "../transactions-account/transactions-account";
 import { CardTransactions } from "../../../shared/card-transactions/card-transactions";
+import { TransactionsButton } from "../../../shared/transactions-button/transactions-button";
 
 @Component({
   selector: 'app-transactions',
-  imports: [TransactionsAccount, CardTransactions],
+  imports: [TransactionsAccount, CardTransactions, TransactionsButton],
   templateUrl: './transactions.html',
   styleUrl: './transactions.scss'
 })
