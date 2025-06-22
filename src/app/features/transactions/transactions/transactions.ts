017592546d27848d3cda0c839b4176769e5a7a16
Clara <clararemotto@gmail.com>
2025-06-22 17:57:52 -0300

    feat: style transaction card

--- src/app/features/transactions/transactions/transactions.ts
+++ src/app/features/transactions/transactions/transactions.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { TransactionsAccount } from "../transactions-account/transactions-account";
 import { CardTransactions } from "../../../shared/card-transactions/card-transactions";
-import { TransactionsButton } from "../../../shared/transactions-button/transactions-button";
 import { TimeFilter } from "../../../shared/time-filter/time-filter";
-import { Actions } from "../../../shared/actions/actions";
+import { TransactionsButton } from "../../../shared/transactions-button/transactions-button";
+import { TransactionsAccount } from "../transactions-account/transactions-account";
 
 @Component({
   selector: 'app-transactions',
-  imports: [TransactionsAccount, CardTransactions, TransactionsButton, TimeFilter, Actions],
+  imports: [TransactionsAccount, CardTransactions, TransactionsButton, TimeFilter],
   templateUrl: './transactions.html',
   styleUrl: './transactions.scss'
 })
