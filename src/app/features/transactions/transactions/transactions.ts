752e3da12ac37aa3df2b28be6b863aeb9330778f
Clara <clararemotto@gmail.com>
2025-06-22 16:57:30 -0300

    feat: style transfer button

--- src/app/features/transactions/transactions/transactions.ts
+++ src/app/features/transactions/transactions/transactions.ts
@@ -3,10 +3,11 @@ import { TransactionsAccount } from "../transactions-account/transactions-accoun
 import { CardTransactions } from "../../../shared/card-transactions/card-transactions";
 import { TransactionsButton } from "../../../shared/transactions-button/transactions-button";
 import { TimeFilter } from "../../../shared/time-filter/time-filter";
+import { Actions } from "../../../shared/actions/actions";
 
 @Component({
   selector: 'app-transactions',
-  imports: [TransactionsAccount, CardTransactions, TransactionsButton, TimeFilter],
+  imports: [TransactionsAccount, CardTransactions, TransactionsButton, TimeFilter, Actions],
   templateUrl: './transactions.html',
   styleUrl: './transactions.scss'
 })
