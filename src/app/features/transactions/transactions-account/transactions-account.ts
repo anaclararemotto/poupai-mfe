b74f7f02705067f238f9743922f8104ee77c3e06
Clara <clararemotto@gmail.com>
2025-06-22 17:40:12 -0300

    feat: style account card of transactions page

--- src/app/features/transactions/transactions-account/transactions-account.ts
+++ src/app/features/transactions/transactions-account/transactions-account.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardInfo } from "../../../shared/card-info/card-info";
+import { Actions } from "../../../shared/actions/actions";
 
 @Component({
   selector: 'app-transactions-account',
-  imports: [CardInfo],
+  imports: [CardInfo, Actions],
   templateUrl: './transactions-account.html',
   styleUrl: './transactions-account.scss'
 })
