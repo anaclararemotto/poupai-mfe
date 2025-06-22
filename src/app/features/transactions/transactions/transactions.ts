8e49565f16442c31ac2dac1e2e1ea991c3f98b7b
Clara <clararemotto@gmail.com>
2025-06-22 15:22:13 -0300

    feat: add time filter button

--- src/app/features/transactions/transactions/transactions.ts
+++ src/app/features/transactions/transactions/transactions.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TransactionsAccount } from "../transactions-account/transactions-account";
 import { CardTransactions } from "../../../shared/card-transactions/card-transactions";
 import { TransactionsButton } from "../../../shared/transactions-button/transactions-button";
+import { TimeFilter } from "../../../shared/time-filter/time-filter";
 
 @Component({
   selector: 'app-transactions',
-  imports: [TransactionsAccount, CardTransactions, TransactionsButton],
+  imports: [TransactionsAccount, CardTransactions, TransactionsButton, TimeFilter],
   templateUrl: './transactions.html',
   styleUrl: './transactions.scss'
 })
