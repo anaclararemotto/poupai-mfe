6920ef4aa5d2925782ab6ba8cdf97a7da63e7bc7
Clara <clararemotto@gmail.com>
2025-07-05 13:40:25 -0300

    feat: create and style footer

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -3,10 +3,11 @@ import { HomeAccount } from "../home-account/home-account";
 import { Actions } from "../../../shared/actions/actions";
 import { CardInfo } from "../../../shared/card-info/card-info";
 import { Navbar } from "../../../shared/navbar/navbar";
+import { Footer } from "../../../shared/footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [HomeAccount, Actions, CardInfo, Navbar],
+  imports: [HomeAccount, Actions, CardInfo, Navbar, Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
