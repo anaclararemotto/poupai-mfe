a0f90646e7a0e6e1afcbfbc9beebe52d857d694d
Clara <clararemotto@gmail.com>
2025-07-05 13:27:17 -0300

    feat: create and style navbar

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HomeAccount } from "../home-account/home-account";
 import { Actions } from "../../../shared/actions/actions";
 import { CardInfo } from "../../../shared/card-info/card-info";
+import { Navbar } from "../../../shared/navbar/navbar";
 
 @Component({
   selector: 'app-home',
-  imports: [HomeAccount, Actions, CardInfo],
+  imports: [HomeAccount, Actions, CardInfo, Navbar],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
