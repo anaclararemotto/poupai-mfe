09ddeaa16d20f9e843607bc6e4cbee2ef57e6bf2
Clara <clararemotto@gmail.com>
2025-07-05 13:43:10 -0300

    feat: apply nav and footer

--- src/app/features/transactions/transactions/transactions.ts
+++ src/app/features/transactions/transactions/transactions.ts
@@ -3,10 +3,12 @@ import { CardTransactions } from "../../../shared/card-transactions/card-transac
 import { TimeFilter } from "../../../shared/time-filter/time-filter";
 import { TransactionsButton } from "../../../shared/transactions-button/transactions-button";
 import { TransactionsAccount } from "../transactions-account/transactions-account";
+import { Navbar } from "../../../shared/navbar/navbar";
+import { Footer } from "../../../shared/footer/footer";
 
 @Component({
   selector: 'app-transactions',
-  imports: [TransactionsAccount, CardTransactions, TransactionsButton, TimeFilter],
+  imports: [TransactionsAccount, CardTransactions, TransactionsButton, TimeFilter, Navbar, Footer],
   templateUrl: './transactions.html',
   styleUrl: './transactions.scss'
 })
