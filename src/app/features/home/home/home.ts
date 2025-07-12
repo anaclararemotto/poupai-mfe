c5bc35e9ee5eb0302f405c4c15e612e50f7f360c
Clara <clararemotto@gmail.com>
2025-07-12 10:19:19 -0300

    feat: create modal transactions

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -6,15 +6,29 @@ import { Navbar } from "../../../shared/navbar/navbar";
 import { Footer } from "../../../shared/footer/footer";
 import { ModalStatement } from "../../../shared/modal-statement/modal-statement";
 import { CommonModule } from '@angular/common';
+import { ModalTransactions } from "../../../shared/modal-transactions/modal-transactions";
 
 @Component({
   selector: 'app-home',
-  imports: [HomeAccount, Actions, CardInfo, Navbar, Footer, ModalStatement, CommonModule],
+  imports: [HomeAccount, Actions, CardInfo, Navbar, Footer, ModalStatement, CommonModule, ModalTransactions],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
 showStatementModal = false
+
+ showModal = false;
+  modalTipo: 'receita' |'despesa' | 'transferencia' | null = null;
+
+  openModal(tipo: 'receita' | 'despesa' | 'transferencia') {
+    this.modalTipo = tipo;
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+    this.modalTipo = null;
+  }
 
 openStatementModal(){
   console.log('Abrindo modal de extrato');
--- src/app/shared/actions/actions.ts
+++ src/app/shared/actions/actions.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-actions',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './actions.html',
   styleUrl: './actions.scss'
 })
 export class Actions {
   @Output() statement = new EventEmitter<void>();
+  @Output() abrirModal = new EventEmitter<'receita' | 'despesa' | 'transferencia'>();
+
+  abrir(tipo: 'receita' | 'despesa' | 'transferencia') {
+    this.abrirModal.emit(tipo);
+  }
 
   onStatement(){
     this.statement.emit();
