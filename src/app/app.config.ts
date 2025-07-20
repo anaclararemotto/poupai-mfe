873870d9ed2b3906e39f4860db4c9c09260105af
Clara <clararemotto@gmail.com>
2025-07-20 08:22:04 -0300

    feat: receive a token from shell

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './core/interceptors/token.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([
+        tokenInterceptor
+      ])
+    ),
   ]
 };
